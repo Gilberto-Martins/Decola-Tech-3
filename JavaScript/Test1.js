f484ccc908793758086ce71eac5ede08150ebe06
Gilberto Martins <gilbertomartins643@gmail.com>
2022-05-15 17:48:29 -0300

    add path JavaScript

---
+++ JavaScript/Test1.js
@@ -0,0 +1,22 @@
+function Impar_par(array) 
+{
+    let evenNumeros = []
+    for (let n = 0; n <= array.length; n++) 
+    {
+        if (array[n] % 2 === 0) 
+        {
+            evenNumeros.push(array[n])   
+        }
+    }
+    console.log(evenNumeros)
+}
+
+
+
+
+
+
+var numeros = [0,1,2,3,4,5,6,7,8,9]
+
+Impar_par(numeros)
+
