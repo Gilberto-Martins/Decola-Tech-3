1c4c33740237265aac8ecbb298f08805f1931506
Gilberto Martins <gilbertomartins643@gmail.com>
2022-05-17 20:04:27 -0300

    Praticando um pouco

---
+++ JavaScript/Variaveis_e_tipos/Constantes.js
@@ -0,0 +1,5 @@
+//Constantes são imutaveis depois de declarada, e não faz hoisting como o var(permite que a variavel seja declarada em qualquer ponto do codigo)
+
+const Primeiro_Nome = "Gilberto"
+
+console.log(Primeiro_Nome)
