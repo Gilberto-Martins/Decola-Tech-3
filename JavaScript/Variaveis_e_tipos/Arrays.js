d7fdd46bd2821bd618dfc29fb6be9373ab493d03
Gilberto Martins <gilbertomartins643@gmail.com>
2022-05-18 21:37:04 -0300

    Arrays

---
+++ JavaScript/Variaveis_e_tipos/Arrays.js
@@ -0,0 +1,9 @@
+let multiplo = [0,1,2,3,4,5,6,7,8,9]
+
+for(let i = 1; i <= 2; i++)
+{
+    for(let e = 1; e <= multiplo.length; e++){
+        console.log(`${i} X ${e} = ${i * e}`)
+    }
+    console.log("\n")
+}
