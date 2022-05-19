822b532fce0efa7eb0074b8872ec618f8afafffb
Gilberto Martins <gilbertomartins643@gmail.com>
2022-05-18 21:09:13 -0300

    Numeros

---
+++ JavaScript/Variaveis_e_tipos/ConheMath.js
@@ -0,0 +1,11 @@
+let num1 = 1.4444
+
+//aredonda para baixo
+console.log(Math.floor(num1))
+
+//mostra o numero completo
+console.log(Math.fround(num1))
+
+//arendoda para cima
+console.log(Math.ceil(num1))
+
