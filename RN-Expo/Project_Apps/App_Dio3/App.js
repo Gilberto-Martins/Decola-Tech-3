45b0761d358d0a3bd392964d0d88841bacfc2fb6
Gilberto Martins <gilbertomartins643@gmail.com>
2022-05-25 00:10:52 -0300

    Refactor: Inplementando Botão com Pressable e Style do mesmo

--- RN-Expo/Project_Apps/App_Dio3/App.js
+++ RN-Expo/Project_Apps/App_Dio3/App.js
@@ -1,22 +1,41 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Image, View, SafeAreaView, Text ,TouchableOpacity} from 'react-native';
+import { StyleSheet, Image, View, SafeAreaView, Text, Pressable, Linking } from 'react-native';
 
 const colorFundoGit = '#010409';
 const imagenGitHub = 'https://avatars.githubusercontent.com/u/59882986?s=96&v=4';
+const handoPressGitHub = 'https://github.com/Gilberto-Martins'
 
 export default function App() {
+
+  const urlGitHub = async () => {
+    console.log('Verificando Link')
+    const resp = await Linking.canOpenURL(handoPressGitHub)
+
+    if(resp){
+      console.log('Link Verificado.')
+      console.log('Abrindo o Link.....')
+      await Linking.openURL(handoPressGitHub)
+    }
+  }
+
+
   return (
+
 
     <SafeAreaView style={styles.container}>
       <View style={styles.content}>
         <StatusBar style='inverted' backgroundColor={colorFundoGit} />
         <Image source={{ uri: imagenGitHub }} style={styles.imgGitHub} />
-        <Text style={[styles.texto, styles.name]}>Gilberto Martins </Text>
-        <Text style={[styles.texto, styles.nickname]}>gilbertomatins</Text>
-        <Text style={[styles.texto, styles.descricao]}>Sou um Universitário da Uninassau. curso: bacharel Ciência da Computação.</Text>
-        <TouchableOpacity onPress={() => alert('Olá Pessoas')}>
-          <Text style={styles.texto}>Ola</Text>
-        </TouchableOpacity>
+        <Text accessibilityLabel='Nome: Gilberto Martins' style={[styles.texto, styles.name]}>Gilberto Martins </Text>
+        <Text accessibilityLabel='NickName: gilbertomartins' style={[styles.texto, styles.nickname]}>gilbertomatins</Text>
+        <Text accessibilityLabel='Descrição: Sou um Universitário da Uninassau. curso: bacharel Ciência da Computação.' style={[styles.texto, styles.descricao]}>Sou um Universitário da Uninassau. curso: bacharel Ciência da Computação.</Text>
+
+        <Pressable onPress={() => urlGitHub()}>
+          <View>
+            <Text style={styles.button}>Open in GitHub</Text>
+          </View>
+        </Pressable>
+
       </View>
     </SafeAreaView>
 
@@ -45,19 +64,27 @@ const styles = StyleSheet.create({
   texto: {
     color: 'white',
   },
-  name:{
+  name: {
     marginTop: 20,
     fontWeight: 'bold',//negrito
     fontSize: 25,
     color: '#a9a9a9'
   },
-  nickname:{
+  nickname: {
     fontSize: 15,
     color: '#474a51'
   },
-  descricao:{
+  descricao: {
     fontWeight: 'bold',// negrito
     fontSize: 17,
     color: '#696969',
+  },
+  button: {
+    backgroundColor: '#494c4e',
+    fontWeight: 'bold',
+    color: '#CDCDCD',
+    borderRadius: 5,
+    padding: 15,
+    marginTop: 25,
   }
 });
